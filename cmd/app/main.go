03136579d58bb855536d28c004aa335614199e89
Tyler <43707450+changty97@users.noreply.github.com>
2025-02-19 15:54:32 -0800

    Initial setup of Go project (#1)
    * Initial setup of Go project
    
    * Adding CI
    
    * Fixed CI
    
    * Fixed CI 2
    
    * Fixed CI 3
    
    * Fixed CI 4
    
    * debug CI
    
    * Fixed CI 5
    
    * Adding yml parser and execute functions

---
+++ cmd/app/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "path/filepath"
+    "gopkg.in/yaml.v2"
+    "os/exec"
+)
+
+type Config struct {
+    Qemu_system_rules Parameters `yaml:"qemu_system_rules"`
+}
+
+type Parameters struct {
+    Scale string `yaml:"scale"`
+    Soc []string `yaml:"soc"`
+    Cpu []string `yaml:"cpu"`
+    Sys_options []Options `yaml:"options"`
+}
+
+type Options struct {
+    Machine string `yaml:"machine"`
+    Gdb string `yaml:"gdb"`
+    Nongraphic string `yaml:"nongraphic"`
+    Kernel string `yaml:"kernel"`
+}
+
+func getYmlFile() *Parameters {
+    filename, _ := filepath.Abs("./example.yml")
+    yamlFile, err := ioutil.ReadFile(filename)
+
+    if err != nil {
+        panic(err)
+    }
+
+    var config Config
+
+    err = yaml.Unmarshal(yamlFile, &config)
+    if err != nil {
+        panic(err)
+    }
+
+    return &config.Qemu_system_rules
+}
+
+func execute(Params Parameters) {
+    
+    fmt.Printf("%#v\n", Params)
+
+    app := "echo"
+    cmd := exec.Command(app, Params.Scale)
+    stdout, err := cmd.Output()
+
+    if err != nil {
+        fmt.Println(err.Error())
+        return
+    }
+
+    fmt.Println(string(stdout))
+}
+
+func main() {
+    file := getYmlFile()
+    execute(*file)
+}
