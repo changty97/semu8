60982d4709d133967887607c35bed0507c25cedb
Tyler Chang <chang.ty97@gmail.com>
2025-02-20 15:48:22 -0800

    Format and linting

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,76 +1,77 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "path/filepath"
-    "gopkg.in/yaml.v2"
-    "os/exec"
-    "strconv"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+
+	"gopkg.in/yaml.v2"
 )
 
 type Config struct {
-    Qemu_system_rules Parameters `yaml:"qemu_system_rules"`
+	Qemu_system_rules Parameters `yaml:"qemu_system_rules"`
 }
 
 type Parameters struct {
-    Scale string `yaml:"scale"`
-    Soc []string `yaml:"soc"`
-    Cpu []string `yaml:"cpu"`
-    Sys_options []Options `yaml:"options"`
+	Scale       string    `yaml:"scale"`
+	Soc         []string  `yaml:"soc"`
+	Cpu         []string  `yaml:"cpu"`
+	Sys_options []Options `yaml:"options"`
 }
 
 type Options struct {
-    Machine string `yaml:"machine"`
-    Gdb string `yaml:"gdb"`
-    Nongraphic string `yaml:"nongraphic"`
-    Kernel string `yaml:"kernel"`
+	Machine    string `yaml:"machine"`
+	Gdb        string `yaml:"gdb"`
+	Nongraphic string `yaml:"nongraphic"`
+	Kernel     string `yaml:"kernel"`
 }
 
 func getYmlFile() *Parameters {
-    filename, _ := filepath.Abs("./example.yml")
-    yamlFile, err := ioutil.ReadFile(filename)
+	filename, _ := filepath.Abs("./example.yml")
+	yamlFile, err := os.ReadFile(filename)
 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-    var config Config
+	var config Config
 
-    err = yaml.Unmarshal(yamlFile, &config)
-    if err != nil {
-        panic(err)
-    }
+	err = yaml.Unmarshal(yamlFile, &config)
+	if err != nil {
+		panic(err)
+	}
 
-    return &config.Qemu_system_rules
+	return &config.Qemu_system_rules
 }
 
 func execute(Params Parameters) {
-    
-    fmt.Printf("%#v\n", Params)
 
-    app := "echo"
+	fmt.Printf("%#v\n", Params)
 
-    scale, err := strconv.Atoi(Params.Scale)
-    if err != nil {
-        panic(err)
-    }
+	app := "echo"
 
-    for i := 0; i < scale; i++ {
-        fmt.Println(i)
-        cmd := exec.Command(app, Params.Scale)
-        stdout, err := cmd.Output()
+	scale, err := strconv.Atoi(Params.Scale)
+	if err != nil {
+		panic(err)
+	}
 
-        if err != nil {
-            fmt.Println(err.Error())
-            return
-        }
+	for i := 0; i < scale; i++ {
+		fmt.Println(i)
+		cmd := exec.Command(app, Params.Scale)
+		stdout, err := cmd.Output()
 
-        fmt.Println(string(stdout))
-    }
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		fmt.Println(string(stdout))
+	}
 }
 
 func main() {
-    file := getYmlFile()
-    execute(*file)
+	file := getYmlFile()
+	execute(*file)
 }
